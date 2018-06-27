6ccd6980aa2ce792890ce956d41aed406a9ec8ff
Usama Qadeer <usama.qadeer90@gmail.com>
2018-06-27 13:13:48 +0500

    first commit

---
+++ Sql Adapter/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sql_Adapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+                 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["testdb"].ConnectionString);
+            SqlCommand com = new SqlCommand("Select * from EntryData", con);
+            con.Open();
+            com.Connection = con;
+            com.ExecuteNonQuery();
+            con.Close();
+
+            SqlDataAdapter sdr = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            sdr.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
